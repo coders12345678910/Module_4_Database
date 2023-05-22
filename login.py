c409caff46b82f90dae1390694aa29d478968276
Muhammad Hasif Bin Muhammad Imran <87602255+MuhammadHasif128@users.noreply.github.com>
2023-05-22 10:49:55 +0800

    test

--- login.py
+++ login.py
@@ -13,6 +13,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'JALALASSS'
 app.config['MYSQL_DB'] = 'pythonlogin'
+app.config['MYSQL_PORT'] = 3306
 
 # Intialize MySQL
 mysql = MySQL(app)
@@ -32,7 +33,7 @@ def login():
 
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,)) # SQL STORING
         # Fetch one record and return result
         account = cursor.fetchone()
 
@@ -42,6 +43,10 @@ def login():
             session['loggedin'] = True
             session['id'] = account['id']
             session['username'] = account['username']
+            # IMPLEMENT SECURITY FEATURE HERE, NOT SECURE
+            # PYTHON HAS A DEFAULT SESSION COOKIE WITH FLASK, NO NID SQL QUERY LIKE THE REQUEST ONE ON TOP
+            # UNDER INSPECT > APPLICATION > SESSION > STORED ON CLIENT
+            # STORING SESSION COOKIE ON LOCAL AND SESSION STORAGE AND TURNING OFF THE DIFF FLAGS CAN BE A SECURITY FEATURE
             # Redirect to home page
             return 'Logged in successfully!'
         else:
@@ -49,5 +54,23 @@ def login():
             msg = 'Incorrect username/password!'
     # Show the login form with message (if any)
     return render_template('index.html', msg='')
+
+
+# http://localhost:5000/MyWebApp/logout - this will be the logout page
+@app.route('/MyWebApp/logout')
+def logout():
+    # Remove session data, this will log the user out
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    # Redirect to login page
+    return redirect(url_for('login'))
+
+
 if __name__ == '__main__':
     app.run()
+
+
+# do not put security features on client side because anyone can go in and turn it off.
+# do on server and client
+# ask elgin for diagram and which one is client and server
